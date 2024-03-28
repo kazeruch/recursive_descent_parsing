cce9fd23a98017a09fd59e0b64b664fc0c18ed93
kazeruch <kazukirock0417@gmail.com>
2024-03-28 10:57:49 +0900

    first commit

---
+++ Number.java
@@ -0,0 +1,15 @@
+public class Number {
+
+    static final String number(String s) {
+        for (int i = 0; i < s.length(); ++i) {
+            if (!Character.isDigit(s.charAt(i))) {
+                return s.substring(0, i);
+            }
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(number("12+34+56"));
+    }
+}
